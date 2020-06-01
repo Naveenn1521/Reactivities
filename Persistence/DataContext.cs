396d57800df443e0adcc058ad99619857b05ded6
Naveenn1521@gmail.com <Naveenn1521@gmail.com>
2020-06-01 19:46:41 -0400

    Initial Changes

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Value> Values { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Value>().HasData(
+                new Value{Id =1,Name="Naveen"},
+                new Value{Id =2,Name="Navya"},
+                new Value{Id =3,Name="Naveen Reddy"},
+                new Value{Id =4,Name="Name"}
+            );
+        }
+    }
+}
